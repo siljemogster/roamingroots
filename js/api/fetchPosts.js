ef2a20955527c06e816aa8e0d09947813f053746
siljemogster <125352923+siljemogster@users.noreply.github.com>
2024-09-10 21:40:17 +0200

    url

--- js/api/fetchPosts.js
+++ js/api/fetchPosts.js
@@ -1,7 +1,7 @@
 import { url } from "../constants.js";
 
 export async function fetchPosts() {
-  const response = await fetch(url);
+  const response = await fetch(`${url}?_embed`);
 
   if (response.ok) {
     const results = await response.json();
--- js/ui/posts/createPost.js
+++ js/ui/posts/createPost.js
@@ -1,19 +1,21 @@
 export function createPost(post) {
+    const { title, excerpt, _embedded, id } = post;
+  
     console.log(post);
   
-    const { title, image, excerpt, } = post;
-  
-    const featured_media = _embedded["wp:featuredmedia"]?.[0]?.media_details.sizes.large.source_url;
+    const featured_media =
+      _embedded["wp:featuredmedia"]?.[0]?.media_details.sizes.large.source_url ||
+      "https://placehold.co/600x400";
   
     const postElement = document.createElement("div");
     postElement.classList.add("post");
   
     const postTitleElement = document.createElement("h2");
-    postTitleElement.textContent = title;
+    postTitleElement.textContent = title.rendered;
   
     const featuredMediaElement = document.createElement("img");
-    imageElement.src = featuredmedia;
-    imageElement.alt = title;
+    featuredMediaElement.src = featured_media;
+    featuredMediaElement.alt = title.rendered;
   
     const postExcerptElement = document.createElement("p");
     postExcerptElement.innerHTML = excerpt;
@@ -29,6 +31,7 @@ export function createPost(post) {
     postElement.append(postExcerptElement);
     postElement.append(link);
   
+    // console.log(postElement);
+  
     return postElement;
   }
-  

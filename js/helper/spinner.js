ad4f31a96c54fb948270035d41522f201950b0f3
siljemogster <125352923+siljemogster@users.noreply.github.com>
2024-09-12 14:13:16 +0200

    spinner

---
+++ js/helper/spinner.js
@@ -0,0 +1,14 @@
+const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', () => {
+
+
+
+    spinnerWrapperEl.style.opacity = '1';
+
+    setTimeout(() => {
+        spinnerWrapperEl.style.display = 'none' ; 
+
+    }, 500);
+
+}); 

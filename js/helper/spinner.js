06a247ccc95ca29b6539c80be19030cee4cbbd30
siljemogster <125352923+siljemogster@users.noreply.github.com>
2024-09-14 13:41:09 +0200

    loading indicator for both the blog posts and the single post page

--- js/helper/spinner.js
+++ js/helper/spinner.js
@@ -1,12 +1,9 @@
 const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
 
 window.addEventListener('load', () => {
-
     spinnerWrapperEl.style.opacity = '1';
 
     setTimeout(() => {
-        spinnerWrapperEl.style.display = 'none' ; 
-
+        spinnerWrapperEl.style.display = 'none';
     }, 500);
-
-}); 
+});
